2150043cdce0fdd5906f71fe3ee1d88adc06fa1f
Rafael Pedrola <rafael.pedrola@adesis.com>
2013-12-16 11:10:03 +0100

    refactor

--- main.js
+++ main.js
@@ -8,20 +8,53 @@ window.requestAnimFrame = (function(){
           };
 })();
 
+//The variables
 var canvas, ctx, centerCoords, radius;
-centerCoords ={x : 0, y: 0}
+centerCoords = {x : 0, y: 0};
 var startingCoords ,radiansMinutes , radiansSeconds ,radiansHours ,obtainPointsSeconds , obtainPointsMinutes ,obtainPointsHours;
     
+//The calculus
 function calculatePointsCircunference(coords, radians){
-        return function(center,variation){
-            return {
-                x: center.x +(radius* Math.cos(Math.atan(coords.x/ coords.y) + radians*variation)),
-                y :  center.y+(radius* Math.sin(Math.atan(coords.x/ coords.y) + radians*variation))
-            }
+    return function(center,variation){
+        return {
+            x: center.x +(radius* Math.cos(Math.atan(coords.x/ coords.y) + radians*variation)),
+            y :  center.y+(radius* Math.sin(Math.atan(coords.x/ coords.y) + radians*variation))
         }
-    };
+    }
+};
 
+
+//Canvas basic functions
+function drawLineFromTo(originCoords, finalCoords){
+    ctx.beginPath();
+    ctx.moveTo(originCoords.x, originCoords.y);
+    ctx.lineTo(finalCoords.x,finalCoords.y);
+    ctx.closePath();
+    ctx.stroke();
+}
+function drawCircle(coords, r){
+    ctx.beginPath();
+    ctx.arc(coords.x, coords.y, r, 0, Math.PI*2, true); 
+    ctx.closePath();
+    ctx.fill();
+}
+function drawCircumference(coords, r){
+    ctx.beginPath();
+    ctx.arc(coords.x, coords.y, r, 0, Math.PI*2, true); 
+    ctx.closePath();
+    ctx.stroke();
+}
+
+//Draw the clock circles
+function drawClock(){
+    drawCircumference(centerCoords, radius);
+    drawCircumference(centerCoords, radius+20);
+    drawCircumference(centerCoords, 10);    
+}
+
+//Render the canvas
 function render(){
+    //Clean the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     var date = new Date();
@@ -35,65 +68,19 @@ function render(){
 
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-
     
-
     var coordS = obtainPointsSeconds(centerCoords, seconds);
     var coordM = obtainPointsMinutes(centerCoords, minutes);
     var coordH = obtainPointsHours(centerCoords, hours);
     
-
-    //draw a circle
-    ctx.beginPath();
-    ctx.arc(centerCoords.x, centerCoords.y, radius, 0, Math.PI*2, true); 
-    ctx.closePath();
-    ctx.stroke();
-    
-    ctx.beginPath();
-    ctx.arc(centerCoords.x, centerCoords.y, radius+20, 0, Math.PI*2, true); 
-    ctx.closePath();
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(centerCoords.x, centerCoords.y, 10, 0, Math.PI*2, true); 
-    ctx.closePath();
-    ctx.fill();
-    
-    ctx.beginPath();
-    ctx.arc(coordS.x, coordS.y, 10, 0, Math.PI*2, true); 
-    ctx.closePath();
-    ctx.fill()
-
+    drawClock();   
     //Seconds
-    ctx.beginPath();
-    ctx.moveTo(centerCoords.x, centerCoords.y);
-    ctx.lineTo(coordS.x,coordS.y);
-    ctx.closePath();
-    ctx.stroke();
-
+    drawLineFromTo(centerCoords, coordS);
+    drawCircle(coordS, 10);
     //Minutes
-    
-    ctx.beginPath();
-    ctx.moveTo(centerCoords.x, centerCoords.y);
-    ctx.lineTo(coordM.x,coordM.y);
-    ctx.closePath();
-    ctx.stroke();
-    
+    drawLineFromTo(centerCoords, coordM);
     //Hours
-    ctx.lineWidth=5;
-    var gradient=ctx.createLinearGradient(0,0,170,0);
-gradient.addColorStop("0","magenta");
-gradient.addColorStop("0.5","blue");
-gradient.addColorStop("1.0","red");
-
-// Fill with gradient
-ctx.strokeStyle=gradient;
-    ctx.beginPath();
-    ctx.moveTo(centerCoords.x, centerCoords.y);
-    ctx.lineTo(coordH.x,coordH.y);
-    ctx.closePath();
-    ctx.stroke();
-   
+    drawLineFromTo(centerCoords, coordH);
    
 }
 
@@ -108,11 +95,21 @@ window.onload = function(){
     centerCoords.x  = window.innerWidth/2;
     centerCoords.y  = window.innerHeight/2;
     radius = window.innerWidth > window.innerHeight ? window.innerWidth/4 : window.innerHeight/4;
-     startingCoords ={ x: -radius, y:0};  
-     radiansMinutes =  radiansSeconds = ((360/60)*2* Math.PI) / 360;
-     radiansHours = ((360/12)*2* Math.PI) / 360;
-     obtainPointsSeconds =  obtainPointsMinutes = calculatePointsCircunference(startingCoords,radiansSeconds);
-     obtainPointsHours = calculatePointsCircunference(startingCoords,radiansHours);
+    startingCoords ={ x: -radius, y:0};  
+    radiansMinutes =  radiansSeconds = ((360/60)*2* Math.PI) / 360;
+    radiansHours = ((360/12)*2* Math.PI) / 360;
+    obtainPointsSeconds =  obtainPointsMinutes = calculatePointsCircunference(startingCoords,radiansSeconds);
+    obtainPointsHours = calculatePointsCircunference(startingCoords,radiansHours);
+    
+    //Style
+    ctx.lineWidth=5;
+    var gradient=ctx.createLinearGradient(0,0,170,0);
+    gradient.addColorStop("0","magenta");
+    gradient.addColorStop("0.5","blue");
+    gradient.addColorStop("1.0","red");
+    // Fill with gradient
+    ctx.strokeStyle=gradient;
+    
     (function animloop(){
       requestAnimFrame(animloop);
       render();
